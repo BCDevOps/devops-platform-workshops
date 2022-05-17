aab4a70311d69b2f2d8480dc02311d9c324ee8e8
Jason Froehlich <jfroehli@redhat.com>
2022-05-17 13:52:59 -0400

    Added my logging app

---
+++ openshift-201/materials/logging/src/main/java/com/mycompany/app/controller/MyLoggingController.java
@@ -0,0 +1,26 @@
+package com.mycompany.app.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+public class MyAppController {
+    private static final java.util.logging.Logger LOGGER = LoggerFactory.getLogger(MyAppController.class);
+
+    private static final String template = "[%d]: Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/hello/{user}")
+    public ResponseEntity<String> hello(@PathParam String user) {
+        long lc = counter.incrementAndGet();
+        LOGGER.info("hello called {} times.", lc);
+
+        return ResponseEntity.ok(String.format(template, lc, user));
+    }
+    
+}
