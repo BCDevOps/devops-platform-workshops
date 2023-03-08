1c43293613443e791df5de7bbdcc5616859354f9
Matt Spencer <99685508+mtspn@users.noreply.github.com>
2023-03-08 13:32:31 -0800

    Update cleanup.sh
    add hpa and vpa

--- openshift-201/instructor-resources/cleanup.sh
+++ openshift-201/instructor-resources/cleanup.sh
@@ -1,7 +1,7 @@
 ## Run as a shell script using `sh cleanup.sh`. 
 LICENCEPLATES=(eca693 ecac3f c2a206 be8d7e ede379 e1f581 aa15e2 c0571b bc454e bba23e b353f4 cf8581 ef3c25 c37719 fe64f4 eb7e66 b80657 e3fa55 f73ace b51b5b cb1c00)
 SUFFIXES=(-dev -test -tools -prod)
-OBJECTS=(all secret configmap pvc el route svc netpol pipelines pipelineruns tasks pdb triggerBindings triggerTemplates)
+OBJECTS=(all secret configmap pvc el route svc netpol pipelines pipelineruns tasks pdb triggerBindings triggerTemplates hpa vpa)
 
 for licenceplate in ${LICENCEPLATES[@]}; do
   for suffix in ${SUFFIXES[@]}; do
