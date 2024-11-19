8cc5e752399670989a106bb28f7d784da5f664ca
Shelly Xue Han <Shelly.Han@gov.bc.ca>
2024-11-18 16:30:56 -0800

    update env var name

--- openshift-201/materials/image-management/sample-app-nodejs/index.js
+++ openshift-201/materials/image-management/sample-app-nodejs/index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 8080;
 
 // Get environment variables with default values
-const envVar1 = process.env.ENV_VAR1 || 'unkonwn';
-const envVar2 = process.env.ENV_VAR2 || '';
-const envVar3 = process.env.ENV_VAR3 || '';
+const envVar1 = process.env.NAME || 'unkonwn';
+const envVar2 = process.env.APP_MSG || '';
+const envVar3 = process.env.SECRET_APP_MSG || '';
 
 // Define a route to return the environment variables as JSON
 app.get('/', (req, res) => {
