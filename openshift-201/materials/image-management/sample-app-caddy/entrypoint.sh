1bb52b3804de28dd4715013e98f55ebce51628e8
Shelly Xue Han <Shelly.Han@gov.bc.ca>
2024-11-18 16:53:10 -0800

    remove unused testing manifest

--- openshift-201/materials/image-management/sample-app-caddy/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Use envsubst to substitute environment variables in the template file
-envsubst < /app/index.html.template > /app/index.html
-
-# Start the Caddy server, pointing to the output directory
-exec /usr/bin/caddy run --config /etc/caddy/Caddyfile
