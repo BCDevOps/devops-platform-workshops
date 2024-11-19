848c51131587c91cad5ccfc66b168ef168c1c310
Shelly Xue Han <Shelly.Han@gov.bc.ca>
2024-11-18 16:20:31 -0800

    update response message

--- openshift-201/materials/image-management/sample-app-nodejs/index.js
+++ openshift-201/materials/image-management/sample-app-nodejs/index.js
@@ -4,16 +4,12 @@ const port = 8080;
 
 // Get environment variables with default values
 const envVar1 = process.env.ENV_VAR1 || 'unkonwn';
-const envVar2 = process.env.ENV_VAR2 || 'default2';
-const envVar3 = process.env.ENV_VAR3 || 'default3';
+const envVar2 = process.env.ENV_VAR2 || '';
+const envVar3 = process.env.ENV_VAR3 || '';
 
 // Define a route to return the environment variables as JSON
 app.get('/', (req, res) => {
-  res.json({
-    ENV_VAR1: envVar1,
-    ENV_VAR2: envVar2,
-    ENV_VAR3: envVar3
-  });
+  res.send(`Hello world from ${envVar1} ${envVar2} ${envVar3}`);
 });
 
 // Start the server
